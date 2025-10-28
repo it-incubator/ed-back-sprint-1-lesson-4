07045bb21248071898cd3a66d97c2856e783d69b
Thedd <mixailmar4uk78@gmail.com>
2025-10-28 13:19:38 +0100

    remove unnecessary `.optional()` in query pagination validation

--- src/core/middlewares/validation/query-pagination-sorting.validation-middleware.ts
+++ src/core/middlewares/validation/query-pagination-sorting.validation-middleware.ts
@@ -22,21 +22,18 @@ export function paginationAndSortingValidation<T extends string>(
 
   return [
     query('pageNumber')
-      .optional()
       .default(DEFAULT_PAGE_NUMBER)
       .isInt({ min: 1 })
       .withMessage('Page number must be a positive integer')
       .toInt(),
 
     query('pageSize')
-      .optional()
       .default(DEFAULT_PAGE_SIZE)
       .isInt({ min: 1, max: 100 })
       .withMessage('Page size must be between 1 and 100')
       .toInt(),
 
     query('sortBy')
-      .optional()
       .default(Object.values(sortFieldsEnum)[0]) // Первое значение enum как дефолтное
       .isIn(allowedSortFields)
       .withMessage(
@@ -44,7 +41,6 @@ export function paginationAndSortingValidation<T extends string>(
       ),
 
     query('sortDirection')
-      .optional()
       .default(DEFAULT_SORT_DIRECTION)
       .isIn(Object.values(SortDirection))
       .withMessage(
